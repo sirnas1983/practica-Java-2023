ebddd4ae89421b70cd23ab8aff9226d09910e863
Darian Hiebl <dahiebl@gmail.com>
2023-09-22 17:39:11 -0300

    ejercicio 3 padawan

--- Padawan/Ejercicio3.java
+++ Padawan/Ejercicio3.java
@@ -1,12 +1,14 @@
 package Padawan;
 
+import java.util.List;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
         
-        char[] name = {'D', 'a', 'r', 'i', 'a', 'n'};
-        for (var i = 0; i < name.length; i++){
-            System.out.println("La letra en la posicion "+i+" es: "+name[i]);
+        // char[] name = {'D', 'a', 'r', 'i', 'a', 'n'};
+        List<Character> name = List.of('D', 'a', 'r', 'i', 'a', 'n');
+        for (var letra : name){
+            System.out.println(letra);
         }
     }
 }
