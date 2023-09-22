27791b5b58b056b5b06e0469382966693d6850d3
Darian Hiebl <dahiebl@gmail.com>
2023-09-22 17:04:21 -0300

    hasta padawan 3

---
+++ Padawan/Ejercicio1.java
@@ -0,0 +1,9 @@
+package Padawan;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int a = 20;
+        a += 20;
+        System.out.println(a);
+    }
+}
---
+++ Padawan/Ejercicio3.java
@@ -0,0 +1,12 @@
+package Padawan;
+
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        
+        char[] name = {'D', 'a', 'r', 'i', 'a', 'n'};
+        for (var i = 0; i < name.length; i++){
+            System.out.println("La letra en la posicion "+i+" es: "+name[i]);
+        }
+    }
+}
