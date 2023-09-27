646b5b614bb4dd610f5d41e6b9391ecfbe15baf2
Darian Hiebl <dahiebl@gmail.com>
2023-09-27 06:55:18 -0300

    mouredev

--- Iniciado/Ejercicio1.java
+++ Iniciado/Ejercicio1.java
@@ -2,22 +2,19 @@ package Iniciado;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-
         public static void main(String[] args) {
-            try{
-                try (Scanner myObj = new Scanner(System.in)) {
+                    Scanner myObj = new Scanner(System.in); 
                     System.out.println("Ingrese un entero: ");
                     int integer1 = myObj.nextInt();
                     System.out.println("Ingrese otro entero: ");
                     int integer2 = myObj.nextInt();
                     int sum = integer1 + integer2;
-                    String mensaje = String.format("El resultado de la suma es %d, es %s",sum, (sum % 2 == 0 ? "par": "impar"));
+                    String mensaje = String.format(
+                        "El resultado de la suma es %d, es %s",
+                        sum, 
+                        sum % 2 == 0 ? "par": "impar"
+                        );
                     System.out.println(mensaje);
-                } }
-            catch(Exception e) {
-                System.out.println(String.format("Ha ocurrido el siguiente error: %s", e.toString()));
-            } finally {
-                
             }
     }
-}
+
